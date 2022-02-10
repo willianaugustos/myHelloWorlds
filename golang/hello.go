a2dfc8e96a81b0672f1fd6ef69b277e61ddb0102
Willian <willian@bytebr.com.br>
2022-02-09 22:35:33 -0300

    Hello World in golang

---
+++ golang/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("(English 🇺🇸) Hello world!")
+	fmt.Println("(Portuguese 🇧🇷) Olá mundo!")
+	fmt.Println("(French 🇫🇷) Bonjour le monde")
+	fmt.Println("(Spanish 🇪🇸) ¡Hola mundo!")
+
+}
